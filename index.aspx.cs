99eb5de7fc73fd71c476beda3f555c220c6ea2b0
Brad Caldwell <caldwebf@mail.uc.edu>
2025-04-08 11:56:21 -0400

    Add project files.

---
+++ index.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InClass04_08_2025
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cmdOK.Click += button1_Click;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "You Clicked!";
+
+        }
+
+    }
+}
